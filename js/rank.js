4a7bbffb1c1578747e10c313fae048be12c84a7e
najaf-mohammed <najafmohammed17@gmail.com>
2020-04-28 05:17:45 +0530

    animations

---
+++ js/rank.js
@@ -0,0 +1,11 @@
+var points={
+    point1:"3", // Go corona
+    point2:"5", // Annamma's Army
+    point3:"0", // Veetil Iri🦚
+    point4:"1", //Nammal powlikyum
+
+    rank_image1:"images/rank2.svg",
+    rank_image2:"images/rank1.svg",
+    rank_image3:"images/rank4.svg",
+    rank_image4:"images/rank3.svg"
+}

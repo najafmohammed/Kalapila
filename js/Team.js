53efa1a4e93c5041365718ed6b049e60c6868365
najaf-mohammed <najafmohammed17@gmail.com>
2020-04-27 22:29:37 +0530

    teams

---
+++ js/Team.js
@@ -0,0 +1,37 @@
+var team1 = { "Team_name" : "Go Corona",
+                member:["Layo",
+                        "Arjun",
+                        "Arya",
+                        "Jeffin",
+                        "Christeena",
+                        "Suby",
+                        "Aswathy"],
+                badge1:["Captain","Vice Captain","Vice Captain"],
+                badge2:["0","0","0","0","0","0","0"]
+            };
+                        
+var team2 = { "Team_name" : "Annamma's Army",
+                        member:["Sneha Anna",
+                        "Mishab ",
+                        "Navneet",
+                        "Likitha",
+                        "Abhinand",
+                        "Nimisha",
+                        "Sreedevi"]};
+var team3 = { "Team_name" : "Veetil Iri",
+                        member:["Nisha ",
+                        "Melvin",
+                        "Pavithra",
+                        "Shishir",
+                        "Sneha Sara",
+                        "Devika ",
+                        "Bipin",
+                        "Renz"]};
+var team4 = { "Team_name" : "Nammal Powlikyum",
+                        member:["Shuhaib ",
+                        "Ananthu ",
+                        "Revathy",
+                        "Reenu",
+                        "Sandra",
+                        "Aparna",
+                        "Minhaj"]};

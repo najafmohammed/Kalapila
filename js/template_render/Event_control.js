01291a13e16f1600d0c9e3168c503fec308e016c
najaf-mohammed <najafmohammed17@gmail.com>
2020-04-29 16:35:56 +0530

    search

--- js/template_render/Event_control.js
+++ js/template_render/Event_control.js
@@ -1,4 +1,13 @@
 var template = $('#EventX').html();
+$(document).ready(function(){
+    console.log("loggable");
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
     var templateScript = Handlebars.compile(template);
     var html = templateScript(eventsx);
     $(document.body).append(html);

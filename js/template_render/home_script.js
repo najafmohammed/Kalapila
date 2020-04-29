8267fa7e7fd09a38b54ed9ea943bfa782089da49
najaf-mohammed <najafmohammed17@gmail.com>
2020-04-29 16:19:53 +0530

    update

--- js/template_render/home_script.js
+++ js/template_render/home_script.js
@@ -1,5 +1,6 @@
 var xxl={"t_name1":rank_names[0]
-,"event_name":next_event_details.event_name
+,"event_name":next_event_details.event_name,
+"progress":"5/18"
 }
   var template = $('#home_info').html();
   var templateScript = Handlebars.compile(template);
